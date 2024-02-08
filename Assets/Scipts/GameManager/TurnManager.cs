f87614d8ff31bafdd99735c7307f840df99e7471
PohangCandy <rlcks777@naver.com>
2024-02-08 20:12:46 +0900

    add Enemy ATQ Map Object

---
+++ Assets/Scipts/GameManager/TurnManager.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurnManager : MonoBehaviour
+{
+    public enum TurnType
+    {
+        EnemyTurn,
+        PlayerTurn
+    }
+}
