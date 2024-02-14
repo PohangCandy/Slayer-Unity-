317553a8fd1ffa8e061948b551e7e54c2b8b6a96
PohangCandy <rlcks777@naver.com>
2024-02-14 11:19:45 +0900

    add Enemy Status Adjustment and push it to Enemy

--- Assets/Scipts/Enemy/EnemyBase.cs
+++ Assets/Scipts/Enemy/EnemyBase.cs
@@ -7,6 +7,11 @@ using StatusAdjustmentInformationNameSpace;
 public class EnemyBase : MonoBehaviour
 {
     int hp;
+    int RandomNumber;
+    int AttackPercentage;
+    int SkillPercentage;
+
+    int SwitchNumber;
     [SerializeField]
     public int power;
     [SerializeField]
@@ -15,11 +20,17 @@ public class EnemyBase : MonoBehaviour
     public TurnManager turnManager;
     [SerializeField]
     public Player target;
+    enum EnemyPatternType { ChargeSkill, ReadyAttack };
     void Start()
     {
+        
         hp = 100;
         power = 10;
         slider.value = hp;
+        AttackPercentage = 60;
+        SkillPercentage = 40;
+        RandomNumber = Random.Range(0, 99);
+        SetPercentagetoValue(RandomNumber);
     }
 
     // Update is called once per frame
@@ -40,6 +51,47 @@ public class EnemyBase : MonoBehaviour
         }
     }
 
+    public void SetPercentagetoValue(int randomNumber)
+    {
+        
+        if (randomNumber - SkillPercentage < 0)
+        {
+            SwitchNumber = 1;
+        }
+        else if(randomNumber - AttackPercentage >= 0)
+        {
+            SwitchNumber = 2;
+        }
+        pattern();
+    }
+
+    void pattern()
+    {
+        switch(SwitchNumber)
+        {
+            case 1:
+                SetNextAction(EnemyPatternType.ChargeSkill);
+                break;
+            case 2:
+                SetNextAction(EnemyPatternType.ReadyAttack);
+                break;
+            default:
+                Debug.Log("Wrong Percentage Value in EnemyPattern");
+                break;
+            }
+    }
+    void SetNextAction(EnemyPatternType nextpattern)
+    {
+        //다음 행동의 패턴 이미지 넣기
+        //다음 행동이 공격일 때 공격력 표시하기
+        //다음 행동이 버프일 때 이미지 표시하기
+    }
+
+    void GetCurAction(EnemyPatternType curpattern)
+    {
+        //현재 적의 턴이라면, 지금 패턴 수행하기
+        //적의 턴이 끝나면 다시 다음 행동 패턴 정하기
+    }
     public void Skill()
     {
         StatusAdjustment.EnemypowerUp(this);
