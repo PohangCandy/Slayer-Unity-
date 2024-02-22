11b8985dbeade42457cd6d5adb5455ebd586df59
PohangCandy <rlcks777@naver.com>
2024-02-22 18:24:37 +0900

    making aqt

--- Assets/Scipts/Antiquity/ATQ_Stat.cs
+++ Assets/Scipts/Antiquity/ATQ_Stat.cs
@@ -2,20 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public class Antiquity
+{
+
+    public Antiquity(string _Name, int _cost, string _description){}
+    string Name;
+    int cost;
+    string description;
+
+
+}
+
+
 public class ATQ_Stat : MonoBehaviour
 {
-    public string Name;
-    public int ID;
-    public int Cost;
-    public string Description;
-    public string Rarity;
+    protected string[] Name = { "콘센트","배터리","수상한전파","수리용렌치","자석 "};
+    protected int[] Cost = { 0 , 100 , 150 , 200 , 250 , 300};
+    protected string[] Description = {}
 
-    ATQ_Stat(string name, int id, int cost, string description, string rarity)
-    {
-        Name = name;
-        ID = id;
-        Cost = cost;
-        Description = description;
-        Rarity = rarity;
-    }
 }
